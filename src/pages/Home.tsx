819e5c255fc1d85fbfa8d46c3b0985fdf51cce3c
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-18 17:05:59 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,15 @@ function Home() {
       <Canvas>
         <XR>
           <ambientLight />
-          <pointLight position={[10, 10, 10]} />
+          <pointLight position={[0.5, 0.5, 0.5]} />
           <directionalLight
             castShadow
-            position={[10, 10, 10]}
+            position={[0.5, 0.5, 0.5]}
             shadow-mapSize-width={1024}
           />
           <Controllers />
           <Hands />
-          <mesh position={[5, 5, 0]}>
+          <mesh position={[0.1, 0.1, 0]}>
             <boxGeometry />
             <meshBasicMaterial color="red" />
           </mesh>
