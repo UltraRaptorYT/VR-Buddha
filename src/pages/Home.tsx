07b6514b60afd34376df95aa1abf152082cf1f67
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-19 02:06:40 +0800

    Loading Animation?

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,19 @@ import { VRButton, XR } from "@react-three/xr";
 import { Canvas } from "@react-three/fiber";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import { useLoader } from "@react-three/fiber";
+import { useState, useEffect } from "react";
 // import Plane from "@/components/3D/Plane";
 
 function Home() {
+  const [showBuddha, setShowBuddha] = useState<boolean>(false);
+
   const obj = useLoader(OBJLoader, "/GuanYin.obj");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowBuddha(true);
+    }, 1000);
+  }, []);
 
   return (
     <>
@@ -15,11 +24,12 @@ function Home() {
         <XR>
           <ambientLight />
           {/* <pointLight position={[0.5, 0.5, 0.5]} /> */}
-          <directionalLight
+          {/* <directionalLight
             castShadow
             position={[0.1, 0.1, 0.1]}
             shadow-mapSize-width={1024}
-          />
+            color={"yellow"}
+          /> */}
           {/* <Controllers />
           <Hands /> */}
           <mesh position={[0, 0.1, 0]}>
@@ -27,11 +37,18 @@ function Home() {
             <meshBasicMaterial color="gold" />
           </mesh>
           <mesh>
-            <primitive
-              object={obj}
-              position={[0, 2.5, -50]}
-              scale={[1.5, 1.5, 1.5]}
-            />
+            {showBuddha ? (
+              <primitive
+                object={obj}
+                position={[0, 2.5, -50]}
+                scale={[1.5, 1.5, 1.5]}
+              />
+            ) : (
+              <>
+                <boxGeometry />
+                <meshBasicMaterial color="blue" />
+              </>
+            )}
           </mesh>
           {/* <Plane color="black" size={{ width: 1, height: 1 }} /> */}
         </XR>
