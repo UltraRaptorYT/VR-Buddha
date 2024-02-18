5b3faf9c0ccda6c97a83b5af4c3fd0fbe7c77bbe
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-19 01:47:00 +0800

    Decrease Y

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ function Home() {
             <boxGeometry />
             <meshBasicMaterial color="gold" />
           </mesh>
-          <primitive object={obj} position={[0, 50, -50]} scale={[1.5, 1.5, 1.5]} />
+          <primitive object={obj} position={[0, 10, -50]} scale={[1.5, 1.5, 1.5]} />
           {/* <Plane color="black" size={{ width: 1, height: 1 }} /> */}
         </XR>
       </Canvas>
