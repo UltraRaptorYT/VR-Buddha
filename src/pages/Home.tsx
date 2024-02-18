cebd895899f5458b6101bdbff0b3d0635139c5d6
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-18 12:15:26 +0800

    Test Box VR

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
+import { Box } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 function Home() {
@@ -7,12 +8,25 @@ function Home() {
       <VRButton />
       <Canvas>
         <XR>
+          <directionalLight
+            castShadow
+            position={[10, 10, 10]}
+            shadow-mapSize-width={1024}
+          />
           <Controllers />
           <Hands />
-          <mesh>
+          {/* <mesh>
             <boxGeometry />
             <meshBasicMaterial color="blue" />
-          </mesh>
+          </mesh> */}
+          <Box
+            position={[0, -0.05, 0]}
+            scale={[100, 0.1, 100]}
+            rotation={[0, 0, 0]}
+            receiveShadow
+          >
+            <meshStandardMaterial color="white" />
+          </Box>
         </XR>
       </Canvas>
     </>
