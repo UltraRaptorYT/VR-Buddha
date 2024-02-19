3100b584d46c41f4e5f81862c4660d5c70de7439
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-19 23:12:54 +0800

    Test SkyBox

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,36 @@
 // import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 import { VRButton, XR } from "@react-three/xr";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader, useThree } from "@react-three/fiber";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
-import { useLoader } from "@react-three/fiber";
+import { CubeTextureLoader } from "three";
 // import { useState, useEffect } from "react";
 // import { Group, Object3DEventMap } from "three";
 // import Plane from "@/components/3D/Plane";
+
+function SkyBox() {
+    const { scene } = useThree();
+    const loader = new CubeTextureLoader();
+    // The CubeTextureLoader load method takes an array of urls representing all 6 sides of the cube.
+    const texture = loader.load([
+      "/1.jpg",
+      "/2.jpg",
+      "/3.jpg",
+      "/4.jpg",
+      "/5.jpg",
+      "/6.jpg",
+    ]);
+    scene.background = texture;
+    return null;
+}
 
 function Home() {
   // const [buddhaOBJ, setBuddhaOBJ] = useState<Group<Object3DEventMap> | null>(
   // const [showBuddha, setShowBuddha] = useState<boolean>(false);
   const buddhaOBJ = useLoader(OBJLoader, "/GuanYin.obj");
 
-  function generateNumber(max: number, min: number = 0) {
-    return Math.random() * max + min;
-  }
+  // function generateNumber(max: number, min: number = 0) {
+  //   return Math.random() * max + min;
+  // }
 
   return (
     <>
@@ -43,8 +59,8 @@ function Home() {
               scale={[1.5, 1.5, 1.5]}
             />
           )}
-
-          {[...new Array(100)].map((idx) => {
+          <SkyBox />
+          {/* {[...new Array(100)].map((idx) => {
             return (
               <mesh
                 key={"star" + idx}
@@ -59,7 +75,7 @@ function Home() {
                 <meshBasicMaterial color="white" />
               </mesh>
             );
-          })}
+          })} */}
           {/* <Plane color="black" size={{ width: 1, height: 1 }} /> */}
         </XR>
       </Canvas>
