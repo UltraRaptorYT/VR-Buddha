6191cfbc40bbf18a6347aa85386fea0d3f9cdf81
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-19 08:18:29 +0800

    Works <3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,39 +4,42 @@ import { Canvas } from "@react-three/fiber";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import { useLoader } from "@react-three/fiber";
 // import { useState, useEffect } from "react";
+// import { Group, Object3DEventMap } from "three";
 // import Plane from "@/components/3D/Plane";
 
 function Home() {
+  // const [buddhaOBJ, setBuddhaOBJ] = useState<Group<Object3DEventMap> | null>(
   // const [showBuddha, setShowBuddha] = useState<boolean>(false);
-
-  const obj = useLoader(OBJLoader, "/GuanYinModel.obj");
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setShowBuddha(true);
-  //   }, 5000);
-  // }, []);
+  const buddhaOBJ = useLoader(OBJLoader, "/GuanYin.obj");
 
   return (
     <>
       <VRButton />
       <Canvas>
         <XR>
-          <ambientLight />
+          <ambientLight intensity={0.055} color={"gold"} />
           {/* <pointLight position={[0.5, 0.5, 0.5]} /> */}
-          {/* <directionalLight
+          <directionalLight
             castShadow
             position={[0.1, 0.1, 0.1]}
             shadow-mapSize-width={1024}
-            color={"yellow"}
-          /> */}
+            color={"white"}
+            intensity={1}
+          />
           {/* <Controllers />
           <Hands /> */}
           <mesh position={[0, 0.1, 0]}>
             <boxGeometry />
             <meshBasicMaterial color="gold" />
           </mesh>
-          <primitive object={obj} position={[0, 10, -50]} scale={[1.5, 1.5, 1.5]} />
+          {buddhaOBJ && (
+            <primitive
+              object={buddhaOBJ}
+              position={[0, 10, -50]}
+              scale={[1.5, 1.5, 1.5]}
+            />
+          )}
+
           {/* <Plane color="black" size={{ width: 1, height: 1 }} /> */}
         </XR>
       </Canvas>
