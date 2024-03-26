885e357683f45b87cbb5a6e56cfddf87db038a35
Soh Hong Yu <sohhongyu@gmail.com>
2024-03-27 00:26:20 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -65,7 +65,7 @@ function Home() {
             intensity={1}
           />
           <SpotlightBeam
-            position={new THREE.Vector3(0, 7.5, -20)}
+            position={new THREE.Vector3(0, 0, -20)}
             rotation={new THREE.Euler(45, 0, 0)}
             scale={new THREE.Vector3(25, 20, 25)}
             opacity={0.5}
