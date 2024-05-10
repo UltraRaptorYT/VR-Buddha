dc82a97a4366749e20c9a9673e6913e322eb1a8f
Soh Hong Yu <sohhongyu@gmail.com>
2024-05-11 01:38:56 +0800

    Snow

--- src/components/3D/Snow.tsx
+++ src/components/3D/Snow.tsx
@@ -15,40 +15,27 @@ function Snow({ count }: { count: number }) {
     const temp = [];
     for (let i = 0; i < count; i++) {
       const t = Math.random() * 100;
-      const factor = 20 + Math.random() * 100;
-      const speed = 0.01 + Math.random() / 200;
       const xFactor = -50 + Math.random() * 100;
-      const yFactor = -50 + Math.random() * 100;
+      const yFactor = 50 + Math.random() * 100; // Adjust yFactor for upward initial movement
       const zFactor = -50 + Math.random() * 100;
-      temp.push({ t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0 });
+      temp.push({ t, xFactor, yFactor, zFactor });
     }
     return temp;
   }, [count]);
   // The innards of this hook will run every frame
   useFrame((_) => {
     particles.forEach((particle, i) => {
-      let { t, factor, speed, xFactor, yFactor, zFactor } = particle;
-      t = particle.t += speed / 2;
-      const a = Math.cos(t) + Math.sin(t * 1) / 10;
-      const b = Math.sin(t) + Math.cos(t * 2) / 10;
-      const s = Math.cos(t);
+      let { t, xFactor, yFactor, zFactor } = particle;
+      t = particle.t += 0.001; // Adjust speed as needed
+
+      // Adjust y position to simulate falling
+      particle.yFactor -= 0.1; // Adjust the value as needed for the falling speed
 
       dummy.position.set(
-        (particle.mx / 10) * a +
-          xFactor +
-          Math.cos((t / 10) * factor) +
-          (Math.sin(t * 1) * factor) / 10,
-        (particle.my / 10) * b +
-          yFactor +
-          Math.sin((t / 10) * factor) +
-          (Math.cos(t * 2) * factor) / 10,
-        (particle.my / 10) * b +
-          zFactor +
-          Math.cos((t / 10) * factor) +
-          (Math.sin(t * 3) * factor) / 10
+        xFactor + Math.cos(t / 10) + Math.sin(t * 1) / 10,
+        yFactor,
+        zFactor + Math.sin(t / 10) + Math.cos(t * 2) / 10
       );
-      dummy.scale.set(s, s, s);
-      dummy.rotation.set(s * 5, s * 5, s * 5);
       dummy.updateMatrix();
 
       if (mesh.current) {
@@ -59,6 +46,7 @@ function Snow({ count }: { count: number }) {
       mesh.current.instanceMatrix.needsUpdate = true;
     }
   });
+
   return (
     <instancedMesh ref={mesh} args={[undefined, undefined, count]}>
       <sphereGeometry attach="geometry" args={[0.075, 0]} />
