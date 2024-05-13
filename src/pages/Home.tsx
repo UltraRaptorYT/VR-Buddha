290c7cca53a473859635fceff6a137c04986e6fd
Soh Hong Yu <sohhongyu@gmail.com>
2024-05-13 18:56:14 +0800

    Make Smaller

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -60,7 +60,7 @@ function Home() {
             <boxGeometry />
             <meshBasicMaterial color="gold" />
           </mesh> */}
-            <Buddha position={[0, 0, -50]} />
+            <Buddha position={[0, -25, -50]} />
             {/* {buddhaOBJ && (
               <primitive
                 object={buddhaOBJ}
