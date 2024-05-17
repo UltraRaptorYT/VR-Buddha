a48f0beee84ae39e3802debeafabfceb90198f64
Soh Hong Yu <sohhongyu@gmail.com>
2024-05-17 14:25:35 +0800

    Move Back

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -89,7 +89,7 @@ function Home({
             <boxGeometry />
             <meshBasicMaterial color="gold" />
           </mesh> */}
-            <Buddha position={[0, -5, -50]} scale={[0.9, 0.9, 0.9]} />
+            <Buddha position={[0, -5, -75]} scale={[0.9, 0.9, 0.9]} />
             {/* {buddhaOBJ && (
               <primitive
                 object={buddhaOBJ}
